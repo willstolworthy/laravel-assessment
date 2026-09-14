de8bcfbc1aff7a07ca39714ec0827a6ba8eeab73
Will Stolworthy <willstolworthy@proton.me>
2026-09-14 15:23:58 +0100

    Feat: styling dashboard sidebar

---
+++ resources/views/components/nav/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="app-grid">
+    <div class="sidebar">
+        <div class="sidebar__branding">
+            <div class="sidebar__branding-logo">WS</div>
+            <span>CRUD</span>
+        </div>
+        <nav class="sidebar__navigation">
+            <a class="sidebar__navigation-link" href="#">Home</a>
+            <a class="sidebar__navigation-link" href="#">Companies</a>
+            <a class="sidebar__navigation-link" href="#">Employees</a>
+        </nav>
+        <div class="sidebar__footer">
+            <div class="sidebar__footer-user">
+                <div class="sidebar__footer-user-logo">A</div>
+                <span class="sidebar__footer-user-name">Admin</span>
+            </div>
+            <div class="sidebar__footer-logout">
+                <a class="sidebar__footer-logout-link">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
