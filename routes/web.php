55939148ea0d5dac2e5260f50b04aa2ff5a181cf
Will Stolworthy <willstolworthy@proton.me>
2026-09-14 12:34:33 +0100

    Feat: installed laravel/ui and removed the ability to register

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->Route('login');
 });
+
+Auth::routes([
+    'register' => false,
+]);
+
+Route::get('/home', [
+    App\Http\Controllers\HomeController::class, 'index'])->name('home');
